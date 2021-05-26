720e6667e62caa13764b4c7f18bf048acb0f4f12
GustavoFerrazRocha1 <gustavoguusfr@gmail.com>
2021-05-26 20:55:54 -0300

    initial commit

---
+++ src/app/services/audio/audio.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AudioService {
+  public audio = new Audio('assets/sounds/8bit.mp3');
+  public muted: boolean;
+  constructor() {
+    this.audio.loop = true;
+    this.muted = true
+  }
+  async defaultAudio(beepId) {
+   this.audio=  new Audio(`assets/sounds/8bit${beepId}.mp3`);
+   this.audio.loop = true;
+   }
+  toggleAudio(){
+    if(!this.muted){
+      this.muted = true;
+      this.audio.muted = true;
+    }else{
+      this.audio.muted = false;
+      this.muted = false;
+      this.audio.play();
+
+    }
+  }
+  getVolume(){
+   return this.audio.volume
+  }
+  setVolume(volume){
+    this.audio.volume = volume / 100
+  }
+  audioPlay(){
+    this.audio.play();
+  }
+}
